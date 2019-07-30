3039d00c66bf8c079e7bb5f87245f383f499133e
uday kumar mailaram <39719416+Chary1729@users.noreply.github.com>
2019-07-30 21:25:14 +0530

    Create s101.py

---
+++ s101.py
@@ -0,0 +1,13 @@
+n=int(input())
+x=[int(j) for j in input().split()]
+rep=[]
+size=len(x)
+for i in range(size):
+  k=i+1
+  for j in range(k,size):
+    if x[i]==x[j] and x[i] not in rep:
+      rep.append(x[i])
+      
+rep.sort()
+print(*rep)
+  
