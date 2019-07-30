37127745f52a81ca9115bf48049d5810daad7fb1
uday kumar mailaram <39719416+Chary1729@users.noreply.github.com>
2019-07-30 21:38:56 +0530

    Update s101.py

--- s101.py
+++ s101.py
@@ -1,17 +1,19 @@
 n=int(input())
 x=[int(j) for j in input().split()]
 rep=[]
+lis=[]
+lis=x
 size=len(x)
-for i in range(size):
-  k=i+1
-  for j in range(k,size):
-    if x[i]==x[j] and x[i] not in rep:
-      rep.append(x[i])
-    else:
-      print('unique')
-      break
-  break
+x=set(x)
+x=list(x)
+if(x==lis):
+  print('unique')
+else:
+  for i in range(size):
+    k=i+1
+    for j in range(k,size):
+      if lis[i]==lis[j] and lis[i] not in rep:
+        rep.append(lis[i])
       
 rep.sort()
 print(*rep)
-  
