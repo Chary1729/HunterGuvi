ee687ad7d8bb29c410e0bcae0e35ebfb2b87cd31
uday kumar mailaram <39719416+Chary1729@users.noreply.github.com>
2019-07-31 13:02:20 +0530

    Update s103.py

--- s103/s103.py
+++ s103/s103.py
@@ -1,8 +1,12 @@
 n=int(input())
 l=[]
+c=0
 x=[int(i) for i in input().split()]
 for i in range(n):
   if(x[i]==i):
-    l.append(i)
-print(*l)
-   
+    l.append(x[i])
+
+if(len(l)==0):
+  print(-1)
+else:
+  print(*l)
